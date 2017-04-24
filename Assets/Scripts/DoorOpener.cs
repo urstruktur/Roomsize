eb731cfd3bd8c244ab4475a836a3af0f26512005
urstruktur <tom@urstruktur.eu>
2017-04-24 22:07:19 +0200

    added most basic game mechanic + colr asset

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DoorOpener : MonoBehaviour
+{
+    [Header("Use \"Door\" tag to animate doors on trigger enter.")]
+
+    public GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject[] doors = GameObject.FindGameObjectsWithTag("Door");
+
+        foreach(GameObject door in doors)
+        {
+            Animation animation = door.GetComponent<Animation>();
+            if (animation != null)
+            {
+                animation.Play("DoorOpening");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GameObject[] doors = GameObject.FindGameObjectsWithTag("Door");
+
+        foreach (GameObject door in doors)
+        {
+            Animation animation = door.GetComponent<Animation>();
+            if (animation != null)
+            {
+                animation.Play("DoorClosing");
+            }
+        }
+    }
+}
