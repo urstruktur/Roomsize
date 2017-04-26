b7a96a69f40e8af54ff40cd8f65aa424d3c924f7
urstruktur <tom@urstruktur.eu>
2017-04-26 22:24:17 +0200

    [UPDATE] position changes of rigidbodys reflect in antipodal room

--- Assets/Scripts/ScaleManager.cs
+++ Assets/Scripts/ScaleManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class ScaleManager : MonoBehaviour {
 
     // the room, where the player is, determines which rigidbodys are kinematic
-    // 
+    //
 
     public GameObject initialRoom;
 
@@ -21,10 +21,13 @@ public class ScaleManager : MonoBehaviour {
     public float doorSize = 0.1f;
 
     private GameObject[] rooms;
+    private Dictionary<Rigidbody, Rigidbody> associations;
 
     void Start () {
         rooms = new GameObject[depth];
+        associations = new Dictionary<Rigidbody, Rigidbody>();
 
+        // create room copy and set position, scale & rotation correctly
         rooms[0] = new GameObject();
         rooms[0].name = "roomCopy" + 1;
         rooms[0].transform.position = doorsillSmall.transform.position;
@@ -32,9 +35,54 @@ public class ScaleManager : MonoBehaviour {
         rooms[0].transform.localScale = new Vector3(1 / doorSize, 1 / doorSize, 1 / doorSize);
         rooms[0].transform.position = doorsillBig.transform.position;
         rooms[0].transform.localRotation = Quaternion.Inverse(doorsillSmall.transform.rotation);
+        InvertKinematic(rooms[0].transform);
+
+        // save associations
+        Rigidbody[] bodiesInitial = initialRoom.GetComponentsInChildren<Rigidbody>();
+        Rigidbody[] bodiesCopy = rooms[0].GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody bodyInitial in bodiesInitial)
+        {
+            foreach (Rigidbody bodyCopy in bodiesCopy)
+            {
+                if(bodyInitial.gameObject.name == bodyCopy.gameObject.name)
+                {
+                    associations.Add(bodyInitial, bodyCopy);
+                }
+            }
+        }
+
+        Debug.Log(associations.Count + " associations saved.");
     }
-	
+
 	void Update () {
-		
-	}
+        foreach (KeyValuePair<Rigidbody, Rigidbody> entry in associations)
+        {
+            Rigidbody a = entry.Value;
+            Rigidbody b = entry.Key;
+
+            if (!a.isKinematic && a.transform.hasChanged)
+            {
+                b.transform.localPosition = a.transform.localPosition;
+                b.transform.localRotation = a.transform.localRotation;
+                a.transform.hasChanged = false;
+            }else if(!b.isKinematic && b.transform.hasChanged)
+            {
+                a.transform.localPosition = b.transform.localPosition;
+                a.transform.localRotation = b.transform.localRotation;
+                b.transform.hasChanged = false;
+            }
+        }
+    }
+
+    // Sets all non-kinematic rigidbodys of children to kinematic rigidbodys and the other way round
+    private void InvertKinematic(Transform t)
+    {
+        Rigidbody[] bodies = t.gameObject.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody body in bodies)
+        {
+            body.isKinematic = !body.isKinematic;
+        }
+    }
+
 }
