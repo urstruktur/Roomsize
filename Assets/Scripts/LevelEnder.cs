080717379efdb499fc41ab0e4a18a6b2e13a253f
urstruktur <tom@urstruktur.eu>
2017-05-10 00:34:42 +0200

    made game winable!

---
+++ Assets/Scripts/LevelEnder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class LevelEnder : MonoBehaviour {
+
+    public GameObject setActiveOnTrigger;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(gameObject.transform.lossyScale.magnitude >= 0.5)
+        {
+            Debug.Log("finish");
+            if (collider.gameObject.GetComponent<FirstPersonController>() != null)
+            {
+                setActiveOnTrigger.SetActive(true);
+            }
+        }
+    }
+}
