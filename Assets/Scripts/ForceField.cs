c9b867b40baabae049f5e4fb789a992e55330e5d
urstruktur <tom@urstruktur.eu>
2017-05-23 23:27:49 +0200

    added force field script + deactivated redundant walls + deactivated blocking box colliders

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Vector3 force = this.transform.position - other.transform.position;
+            force.Normalize();
+            other.GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+}
