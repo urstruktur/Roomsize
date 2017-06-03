83ed9499526e9a0139613fbe648d08b71053a761
urstruktur <tom@urstruktur.eu>
2017-06-03 22:46:57 +0200

    added menu functionality

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void LoadLevel(int index)
+    {
+        Debug.Log("Loading level " + index);
+        SceneManager.LoadScene(index);
+    }
+}
